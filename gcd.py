e614a1f1c45f535e9d24b01b3bbb55aba6ce14f7
GCollado <gabriel.collado@spsmail.cuny.edu>
2019-12-17 02:12:20 -0500

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,17 @@
+"""
+===========================================
+Part 2 - Implement Euclid's GCD Algorithm
+===========================================
+1. Create a recursive Greatest common divisor (the largest number that two integers can be divided
+ by without a remainder)
+2. Called the recursive function "gcd" that takes in parameters a, b,
+"""
+
+def gcd(a, b):
+    if a == 0:
+        return b
+    else:
+        return gcd(b%a, a)
+
+if __name__ == '__main__':
+    print(gcd(252,105))

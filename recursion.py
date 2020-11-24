b692e39189ea31542f225d15b8444c6e26014b23
GCollado <gabriel.collado@spsmail.cuny.edu>
2020-11-23 23:59:31 -0500

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -3,27 +3,20 @@
 Part 1 - Implement the Fibonacci Sequence
 ===========================================
 1. Create a recursive Fibonacci sequence (nth number is the sum of the previous two in the sequence)
-2. Save the function in a file call recursion.py, name the function Fibonacci
+2. Save the function in a file call recursion.py, name the function fibonacci
 """
-from functools import lru_cache
-import sys
 
-#F(n) = F(n-1) + F(n-2)
 
-@lru_cache(maxsize = 1000)
 def fibonacci(n):
-    sys.setrecursionlimit(10000)
-    if type(n) !=int:
-        raise TypeError("n must be a positive number")
-    a = n - 1
-    b = n - 2
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(a) + fibonacci(b)
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        else:
+            return fibonacci(n -1) + fibonacci(n - 2)
 
-#0,1,1,,2,3,5,8,13,21,34
+
 if __name__ == '__main__':
-    print(fibonacci(500))
+    print(fibonacci(5))
 
 
